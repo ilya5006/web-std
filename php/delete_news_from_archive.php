ea59eea1a53a113a4f6d2288518c05d14f830d6c
ilya5006 <samarcev.ilya@mail.ru>
2020-02-16 21:42:10 +0300

    Minor spaces

--- php/delete_news_from_archive.php
+++ php/delete_news_from_archive.php
@@ -1,5 +1,5 @@
 <?php
-	require_once __DIR__ . './connection.php';
+    require_once __DIR__ . './connection.php';
 
     $idNews = htmlentities($connection->escapeString($_GET['id_news']));
     $imagePathDatabase = $connection->query("SELECT `photo_news` FROM `news` WHERE `id_news` = '$idNews'")['photo_news'];
