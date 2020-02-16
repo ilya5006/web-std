bf24138121c66644d3feba5854ed67348d13a895
ilya5006 <samarcev.ilya@mail.ru>
2020-02-16 21:41:27 +0300

    Added filter

--- php/delete_feedback.php
+++ php/delete_feedback.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__ . './connection.php';
-    $idFeedback = $_GET['id_feedback'];
+    $idFeedback = htmlentities($connection->escapeString($_GET['id_feedback']));
 
     $connection->queryExecute("DELETE FROM `feedbacks` WHERE `id_feedback` = '$idFeedback'");
 
